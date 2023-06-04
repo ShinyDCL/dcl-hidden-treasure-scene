28f56b39be80e0a69f7e3cd6035cb7caab1c76c8
Ingaaa <106094638+ShinyDCL@users.noreply.github.com>
2023-06-04 19:20:07 +0300

    feat: add github and twitter links

--- src/game.ts
+++ src/game.ts
@@ -24,3 +24,31 @@ createEntity(
   { position: new Vector3(48, 0, 48), rotation: Quaternion.Euler(0, 180, 0) },
   'models/scene.glb'
 );
+
+// Github link
+const githubLink = createEntity(
+  { position: new Vector3(52, 1.3, 30) },
+  'models/default/githubLink.glb'
+);
+githubLink.addComponent(
+  new OnPointerDown(
+    () =>
+      openExternalURL('https://github.com/ShinyDCL/dcl-hidden-treasure-scene'),
+    { hoverText: 'Github' }
+  )
+);
+
+// Twitter link
+const twitterLink = createEntity(
+  { position: new Vector3(53, 1.3, 30) },
+  'models/default/twitterLink.glb'
+);
+twitterLink.addComponent(
+  new OnPointerDown(
+    () =>
+      openExternalURL(
+        'https://twitter.com/ShinyDCL/status/1665392365064814592'
+      ),
+    { hoverText: 'Twitter' }
+  )
+);
